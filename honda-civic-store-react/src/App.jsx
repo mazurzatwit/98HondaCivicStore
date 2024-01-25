5619938eac35c6a76c45d6f2920b3e3c6582af35
mazurzatwit <mazurz1014@gmail.com>
2024-01-25 16:46:56 -0500

    products data entered

--- honda-civic-store-react/src/App.jsx
+++ honda-civic-store-react/src/App.jsx
@@ -1,16 +1,28 @@
-import NavigationBar from './NavigationBar';
-import { Routes, Route } from 'react-router-dom';
-import Product from './Product';
-import './App.css';
+import NavigationBar from "./NavigationBar";
+import { Routes, Route } from "react-router-dom";
+import Product from "./Product";
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+      const getData = async function () {
+        const response = await fetch("http://localhost:5173/data.json");
+        const data = await response.json();
+        setData(data);
+      };
+    getData();
+  }, []);
+
   return (
     <div>
       <h1>1998 Honda Civic Store</h1>
       <NavigationBar />
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/product" element={<Product />} />
+        <Route path="/product" element={<Product {...data}/>} />
         <Route path="/accessories" element={<h1>Accessories</h1>} />
         <Route path="/gallery" element={<h1>Gallery</h1>} />
         <Route path="/cart" element={<h1>Cart</h1>} />
--- honda-civic-store-react/src/Product.jsx
+++ honda-civic-store-react/src/Product.jsx
@@ -1,4 +1,39 @@
-function Product() {
-  return <p>it&apos;s a 1998 Honda Civic</p>;
+function Product(props) {
+  if(!props.products){
+    return false;
+  }
+  return (
+    <div>
+      <h4>Products</h4>
+      <div>
+        <p>{props.products[0].productName}</p>
+        <p>{props.products[0].price}</p>
+        <p>{props.products[0].description}</p>
+      </div>
+    </div>
+  );
 }
 export default Product;
+
+/*
+async function productPage() {
+  try {
+    const products = await fetchProducts();
+
+    document.getElementById("page-title").innerHTML = "Products";
+
+    const productList = products.map((product) => {
+      return `<tr><td>${product.productName}</td><td>${product.description}</td><td>${product.price}</td></tr>`;
+    });
+    // const productHTMLList = productList.join("");
+
+    const tableHeader =
+      "<tr><th>Product Name </th><th>Product Desc </th><th>Product price </th></tr>";
+    document.getElementById("app").innerHTML =
+      "<table>" + tableHeader + productList + "</table>";
+  } catch (error) {
+    console.error("error with fetch", error);
+  }
+}
+export default productPage;
+*/
